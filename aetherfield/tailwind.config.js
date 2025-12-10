1900e4502d82c0f6b13ec702fe71c33b21624b52
Frank20060 <159519815+Frank20060@users.noreply.github.com>
2025-12-10 19:57:09 +0000

    inicio (no he hecho nada)

---
+++ aetherfield/tailwind.config.js
@@ -0,0 +1,14 @@
+
+///Aqui pongo las variables que quiero personalizar
+
+
+tailwind.config = {
+        theme: {
+        extend: {
+            colors: {
+            "brand-primary": "oklch(79.2% 0.209 151.711)",
+            "brand-secondary": "oklch(82.8% 0.189 84.429)",
+            },
+        },
+        },
+    };
